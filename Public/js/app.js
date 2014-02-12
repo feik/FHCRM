ea86fb48bb48df942e3b9e5f20c43d7d1d88de01
focalhot <focalhot@hotmail.com>
2014-02-12 15:47:08 +0800

    require方法引用

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,4 +1,10 @@
 Ext.Loader.setConfig({enabled:true});
+Ext.Loader.setPath({
+	'Ext.ux':'Public/lib/extjs/ux'
+});
+Ext.require([
+	'Ext.ux.form.CkEditor'
+]);
 Ext.application({
 	name: 'app',
 	appFolder: 'Public/js/app',
